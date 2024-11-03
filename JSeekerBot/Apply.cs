e42a6fc8970e7499459e7e218896e625b66b4e7f
Johnny Arnett <im10g@hotmail.com>
2024-11-02 19:43:47 -0500

    Lots of code improvements and quality of life. Overall works, some edge cases still causing failures rarely, and haven't figured out the verification fully

--- JSeekerBot/Apply.cs
+++ JSeekerBot/Apply.cs
@@ -1,11 +1,64 @@
+using System.Collections;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Drawing.Charts;
 using JSeekerBot.Configuration;
 using Microsoft.Playwright;
+using Tensorflow;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace JSeekerBot
 {
     [TestFixture]
     public class ApplyForJobsEasily : PageTest
     {
+        private List<JobApplicationStatusEntry> _applicationStatusEntries = new List<JobApplicationStatusEntry>();
+        private ResponseInterpreter _responseInterpreter;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _responseInterpreter = new ResponseInterpreter();
+            _responseInterpreter.AddQuestionResponse("C#", "10");
+            _responseInterpreter.AddQuestionResponse("bachelor", "No");
+            _responseInterpreter.AddQuestionResponse("Bachelor", "No");
+            _responseInterpreter.AddQuestionResponse("background check", "No");
+            _responseInterpreter.AddQuestionResponse("phone number", "8123455974");
+            _responseInterpreter.AddQuestionResponse("email", "im10g@hotmail.com");
+            _responseInterpreter.AddQuestionResponse("Email", "im10g@hotmail.com");
+            _responseInterpreter.AddQuestionResponse("visa", "No");
+            _responseInterpreter.AddQuestionResponse("legally authorized to work", "yes");
+            _responseInterpreter.AddQuestionResponse("Automation", "5");
+            _responseInterpreter.AddQuestionResponse("automation", "5");
+            _responseInterpreter.AddQuestionResponse("JavaScript", "5");
+            _responseInterpreter.AddQuestionResponse("XML", "5");
+            _responseInterpreter.AddQuestionResponse("onsite", "Yes");
+            _responseInterpreter.AddQuestionResponse("SQL", "8");
+            _responseInterpreter.AddQuestionResponse("Oracle", "2");
+            _responseInterpreter.AddQuestionResponse("Powershell", "3");
+            _responseInterpreter.AddQuestionResponse("Jira", "4");
+            _responseInterpreter.AddQuestionResponse("hybrid", "4");
+            _responseInterpreter.AddQuestionResponse("Software as a Service", "3");
+            _responseInterpreter.AddQuestionResponse("SaaS", "3");
+            _responseInterpreter.AddQuestionResponse("Java", "3");
+            _responseInterpreter.AddQuestionResponse("Full-Stack", "6");
+            _responseInterpreter.AddQuestionResponse("Web Application", "10");
+            _responseInterpreter.AddQuestionResponse("commuting", "Yes");
+            _responseInterpreter.AddQuestionResponse("AWS", "5");
+            _responseInterpreter.AddQuestionResponse("Amazon Web Services", "5");
+            _responseInterpreter.AddQuestionResponse("JQUERY", "10");
+            _responseInterpreter.AddQuestionResponse("HTML", "10");
+            _responseInterpreter.AddQuestionResponse(".NET", "10");
+            _responseInterpreter.AddQuestionResponse("react", "2");
+            _responseInterpreter.AddQuestionResponse("first name", "Johnny");
+            _responseInterpreter.AddQuestionResponse("last name", "Arnett");
+            _responseInterpreter.AddQuestionResponse("First name", "Johnny");
+            _responseInterpreter.AddQuestionResponse("Last name", "Arnett");
+            _responseInterpreter.AddQuestionResponse("phone", "8123455974");
+            _responseInterpreter.AddQuestionResponse("Phone", "8123455974");
+        }
+
         [Test]
         public async Task ApplyNow()
         {
@@ -15,7 +68,6 @@ namespace JSeekerBot
             await Page.WaitForURLAsync("https://www.linkedin.com/login");
 
             //Login
-
             var email = TestConfig.GetVarFor<string>("LINKEDIN_USERNAME");
             var pass = TestConfig.GetVarFor<string>("LINKEDIN_PASSWORD");
 
@@ -23,12 +75,31 @@ namespace JSeekerBot
             await Page.GetByLabel("Email or phone").FillAsync(email);
             await Page.GetByLabel("Password").FillAsync(pass);
             await Page.GetByRole(AriaRole.Button, new () { NameString = "Sign in" }).ClickAsync();
+
+            await Page.WaitForTimeoutAsync(30000);
+
+            await Page.ClickAsync("body", new PageClickOptions()
+            {
+                Position = new Position()
+                {
+                    X = 625,
+                    Y = 480
+                }
+            });
+
+            await Page.ScreenshotAsync(new()
+            {
+                Path = "verification.png",
+            });
+
+
             await Page.WaitForURLAsync("https://www.linkedin.com/feed/");
+
 
             //Navigate to the Find Jobs page. 
             await Page.GetByRole(AriaRole.Link, new () { NameString = "Jobs" }).ClickAsync();
             await Page.WaitForURLAsync("https://www.linkedin.com/jobs/");
-            await Page.GetByRole(AriaRole.Link, new () { NameString = "Show all {:headerTitle}" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Link, new () { NameString = "Show all More jobs for you" }).ClickAsync();
 
 
             var currentPageCounter = 1;
@@ -54,6 +125,27 @@ namespace JSeekerBot
                             await TryToSubmitApplication();
                             await Page.WaitForTimeoutAsync(1500);
                         }
+                        else
+                        {
+                            string? companyTitle = "";
+
+                            if (await Page.Locator("#jobs-apply-header").IsVisibleAsync())
+                            {
+                                companyTitle = await Page.Locator("#jobs-apply-header").TextContentAsync();
+                            }
+
+                            //var compensation = await Page.GetByText(new Regex("K\\/yr")).InnerTextAsync();
+
+                            var jobTitle = await Page.Locator(".job-details-jobs-unified-top-card__job-title").TextContentAsync();
+
+                            if (companyTitle != null)
+                            {
+                                string replaceWith = "";
+                                var cleanCompanyTitle = companyTitle.Replace("Apply to", "").Replace("\n", replaceWith);
+                                var cleanJobTitle = jobTitle.Replace("\n", replaceWith);
+                                _applicationStatusEntries.Add(new JobApplicationStatusEntry(cleanCompanyTitle, false, DateTime.UtcNow, cleanJobTitle, false));
+                            }
+                        }
                     }
                 }
 
@@ -77,8 +169,19 @@ namespace JSeekerBot
         private async Task TryToSubmitApplication()
         {
             int tryCounter = 0;
+            string? companyTitle = "";
+
+            if (await Page.Locator("#jobs-apply-header").IsVisibleAsync())
+            {
+                companyTitle = await Page.Locator("#jobs-apply-header").TextContentAsync();
+            }
+
+            var jobTitle = await Page.Locator(".job-details-jobs-unified-top-card__job-title").TextContentAsync();
+            bool successfullyApplied = false;
+
             while (await Page.Locator($"[aria-label='Continue to next step']").IsVisibleAsync() && tryCounter < 15)
             {
+
                 await TryFillEmptyInputs();
 
                 if (await Page.Locator($"[aria-label='Continue to next step']").IsVisibleAsync() && await Page.Locator($"[aria-label='Continue to next step']").CountAsync() > 0 && await Page.Locator($"[aria-label='Continue to next step']").IsEnabledAsync())
@@ -93,9 +196,20 @@ namespace JSeekerBot
                 if (await Page.GetByRole(AriaRole.Button, new() { NameString = "Review your application" }).IsVisibleAsync())
                     await Page.GetByRole(AriaRole.Button, new () { NameString = "Review your application" }).ClickAsync();
 
-                if (await Page.GetByRole(AriaRole.Button, new () { NameString = "Submit application" }).IsVisibleAsync())
+                if (await Page.GetByRole(AriaRole.Button, new() { NameString = "Submit application" }).IsVisibleAsync())
+                {
                     await Page.GetByRole(AriaRole.Button, new() { NameString = "Submit application" }).ClickAsync();
+                    successfullyApplied = true;
+                }
 
+            }
+
+            if (companyTitle != null)
+            {
+                string replaceWith = "";
+                var cleanCompanyTitle = companyTitle.Replace("Apply to", "").Replace("\n", replaceWith);
+                var cleanJobTitle = jobTitle.Replace("\n", replaceWith);
+                _applicationStatusEntries.Add(new JobApplicationStatusEntry(cleanCompanyTitle, successfullyApplied, DateTime.UtcNow, cleanJobTitle, true));
             }
 
             await CloseJobDetails();
@@ -112,9 +226,24 @@ namespace JSeekerBot
 
             foreach (var input in inputs)
             {
-                if(await input.InputValueAsync() == string.Empty)
+                //Get the textboxes question
+                var inputId = await input.GetAttributeAsync("Id");
+                var questionLabelElement = Page.Locator($"[for='{inputId}']");
+
+                string question = "";
+
+                if (await questionLabelElement.IsVisibleAsync())
+                    question = await questionLabelElement.InnerTextAsync();
+
+                var response = _responseInterpreter.GetQuestionResponse(question);
+
+                if (response != null)
                 {
-                    await input.FillAsync("4");
+                    await input.FillAsync(response);
+                }
+                else
+                {
+                    await input.FillAsync("5");
                 }
             }
 
@@ -122,13 +251,71 @@ namespace JSeekerBot
 
             foreach (var box in comboBoxes)
             {
+                var options = await box.QuerySelectorAllAsync("option");
+                //Get the textboxes question
+                var inputId = await box.GetAttributeAsync("Id");
+
+
+                var questionLabelElement = Page.Locator($"[for='{inputId}']");
+
+                string question = "";
+
+                if (await questionLabelElement.IsVisibleAsync())
+                    question = await questionLabelElement.InnerTextAsync();
+
+                var response = _responseInterpreter.GetQuestionResponse(question);
+
                 if (await box.InputValueAsync() == "" || await box.InputValueAsync() == "Select an option")
                 {
-                    await box.SelectOptionAsync(new[] { "Yes" });
+                    if (response != null)
+                    {
+                        var isInt = int.TryParse(response, out int result);
+
+                        if (!isInt)
+                        {
+                            await box.SelectOptionAsync(response);
+                        }
+                        else
+                        {
+                            await box.SelectOptionAsync(await options[1].GetAttributeAsync("value"));
+                        }
+
+                    }
+                    else
+                    {
+                        await box.SelectOptionAsync(await options[1].GetAttributeAsync("value") );
+                    }
+                }
+            }
+
+            var radioButtonQuestionContainer = await Page.QuerySelectorAllAsync("fieldset");
+
+            foreach (var question in radioButtonQuestionContainer)
+            {
+                //Get the textboxes question
+                var questionText = await (await question.QuerySelectorAsync("legend")).InnerTextAsync();
+                var response = _responseInterpreter.GetQuestionResponse(questionText);
+                var options = await question.QuerySelectorAllAsync("label");
+
+                if (response != null)
+                {
+                    if (response == "yes")
+                    {
+                        await options[0].ClickAsync();
+                    }
+                    else
+                    {
+                        await options[1].ClickAsync();
+                    }
+                }
+                else
+                {
+                    await options.FirstOrDefault().ClickAsync();
                 }
             }
         }
 
+        /// <summary>
         /// <summary>
         /// Closes out of the Easy Apply Form. Used when unable to submit, or after app submission.
         /// Used to keep flow when things don't work exactly as expected.
@@ -146,6 +333,51 @@ namespace JSeekerBot
             {
                 await Page.GetByRole(AriaRole.Button, new () { NameString = "Discard" }).ClickAsync();
             }
+        }
+
+        [OneTimeTearDown]
+        public void GenerateExcelDocument()
+        {
+            var wb = new XLWorkbook();
+            var workSheet = wb.AddWorksheet($"Job_Application_Record");
+
+            workSheet.Cell(1, 1).Value = "Company";
+            workSheet.Cell(1, 2).Value = "Job Title";
+            workSheet.Cell(1, 3).Value = "Date Application Attempted";
+            workSheet.Cell(1, 4).Value = "Application Status";
+            workSheet.Cell(1, 5).Value = "Had Easy Apply";
+
+            for (int i = 0; i < _applicationStatusEntries.Count; i++)
+            {
+                workSheet.Cell(i+2, 1).Value = _applicationStatusEntries[i].CompanyName;
+                workSheet.Cell(i+2, 2).Value = _applicationStatusEntries[i].JobTitle;
+                workSheet.Cell(i+2, 3).Value = _applicationStatusEntries[i].DateAttempted;
+                workSheet.Cell(i+2, 4).Value = _applicationStatusEntries[i].ApplicationStatus;
+                workSheet.Cell(i+2, 5).Value = _applicationStatusEntries[i].HadEasyApplyButton;
+            }
+
+            wb.SaveAs($"ApplicationStatuses - {DateTime.UtcNow.Ticks}.xlsx");
+        }
+
+
+    }
+
+    public class JobApplicationStatusEntry()
+    {
+
+        public string CompanyName { get; set; }
+        public bool ApplicationStatus { get; set; }
+        public DateTime DateAttempted { get; set; }
+        public string JobTitle { get; set; }
+        public bool HadEasyApplyButton { get; set; }
+
+        public JobApplicationStatusEntry(string companyName, bool applicationStatus, DateTime dateAttempted, string jobTitle, bool hadEasyApply) : this()
+        {
+            this.CompanyName = companyName;
+            this.ApplicationStatus = applicationStatus;
+            this.DateAttempted = dateAttempted;
+            this.JobTitle = jobTitle;
+            this.HadEasyApplyButton = hadEasyApply;
         }
     }
 }
---
+++ JSeekerBot/ResponseInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSeekerBot
+{
+    public class ResponseInterpreter
+    {
+        private Dictionary<string, string> questionResponseDictionary = new Dictionary<string, string>();
+
+        public void AddQuestionResponse(string questionKeyword, string correctResponse)
+        {
+            questionResponseDictionary.Add(questionKeyword, correctResponse);
+        }
+
+        public string? GetQuestionResponse(string question)
+        {
+            foreach (var questionResponsePair in questionResponseDictionary)
+            {
+                if (question.Contains(questionResponsePair.Key))
+                {
+                    return questionResponsePair.Value;
+                }
+            }
+
+            return null;
+        }
+    }
+}
